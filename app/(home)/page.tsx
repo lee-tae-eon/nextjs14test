51217cc8413523754e80da7e506e08123c7b6e7f
이태언 <mktears0609@gmail.com>
2024-11-05 06:36:57 +0900

    ADD: comment

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+export default function RootPage() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+/**
+ * * route group 은 (home) 소괄호 형식으로 묶어줘야한다
+ * * 소괄호로 묶은 경우 route로 인식 하지 않는다
+ * * layout, not-found 는 모든 페이지에 공유
+ * */
