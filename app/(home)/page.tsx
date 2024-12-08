7d483cfdc8f6f1b2d4d8ebaeddbbb39a11a79431
이태언 <mktears0609@gmail.com>
2024-12-08 16:05:35 +0900

    TEST: server

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,14 @@
-"use client";
-import { useEffect, useState } from "react";
+const URI = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+export default function Page() {
+  return <div></div>;
+}
+
+/**
+ * 클라이언트 사이드 방식
+ */
+// "use client";
+// import { useEffect, useState } from "react";
 
 // * page or layout 만 metadata 를 내보낼 수 있다.
 // * metadata 는 서버 컴포넌트에만 있을 수 있다.
@@ -8,27 +17,27 @@ import { useEffect, useState } from "react";
 //   description: "Text nextjs metadata",
 // };
 
-export default function RootPage() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
+// export default function RootPage() {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    async function getMovies() {
-      const response = await fetch(
-        "https://nomad-movies.nomadcoders.workers.dev/movies"
-      );
+//   useEffect(() => {
+//     async function getMovies() {
+//       const response = await fetch(
+//         "https://nomad-movies.nomadcoders.workers.dev/movies"
+//       );
 
-      const json = await response.json();
+//       const json = await response.json();
 
-      setMovies(json);
-      setIsLoading(false);
-    }
+//       setMovies(json);
+//       setIsLoading(false);
+//     }
 
-    getMovies();
-  }, [setMovies, setIsLoading]);
+//     getMovies();
+//   }, [setMovies, setIsLoading]);
 
-  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
-}
+//   return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
+// }
 
 /**
  * * route group 은 (home) 소괄호 형식으로 묶어줘야한다
